b5717402178438d44c4abd9f32bea1aa6ce9314c
eloahbaracho <eloahbarachodossantos@gmail.com>
2024-03-16 11:11:16 -0300

    commit 02

--- app/src/main/java/com/example/listacontatos/activityPrincipal.java
+++ app/src/main/java/com/example/listacontatos/activityPrincipal.java
@@ -1,12 +1,25 @@
 package com.example.listacontatos;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class activityPrincipal extends AppCompatActivity {
     public void onCreate(Bundle bundle) {
-
         super.onCreate(bundle);
+        EditText editNome = findViewById(R.id.camponome);
+        EditText editEmail = findViewById(R.id.campoemail);
+        EditText editTel = findViewById(R.id.campotelefone);
+        Button salvar = findViewById(R.id.salvar);
+        salvar.setOnClickListener( e -> {
+                    Log.i("Agenda Contato", "Nome: " + editNome.getText());
+                    Log.i("Agenda Contato", "Email: " + editEmail.getText());
+                    Log.i("Agenda contato", "Telefone: " + editTel.getText());
+                }
+                );
+        setContentView(R.layout.layout_contatos);
     }
 }

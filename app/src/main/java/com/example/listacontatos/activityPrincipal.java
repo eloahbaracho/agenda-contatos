e5f0c9514c13ac2d58eba779cb2110b734a0ead8
eloahbaracho <eloahbarachodossantos@gmail.com>
2024-03-16 10:44:28 -0300

    commit 01

---
+++ app/src/main/java/com/example/listacontatos/activityPrincipal.java
@@ -0,0 +1,12 @@
+package com.example.listacontatos;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class activityPrincipal extends AppCompatActivity {
+    public void onCreate(Bundle bundle) {
+
+        super.onCreate(bundle);
+    }
+}
